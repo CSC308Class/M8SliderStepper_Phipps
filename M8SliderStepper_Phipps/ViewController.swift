4eca9fb21ead2f424eb41eaab21595c89883abea
Makendra <gamerchan756@gmail.com>
2026-03-24 12:15:58 -0400

    Sliders

--- M8SliderStepper_Phipps/ViewController.swift
+++ M8SliderStepper_Phipps/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    
+    @IBOutlet weak var stepperLabel: UILabel!
+    
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        sliderLabel.text = "\(Int(slider.value))"
+    }
+    
+    
+    @IBAction func stepperChanged(_ sender: Any) {
+        stepperLabel.text = "\(Int(stepper.value))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sliderLabel.text = "\(Int(slider.value))"
+        stepperLabel.text = "\(Int(stepper.value))"
     }
 
 
